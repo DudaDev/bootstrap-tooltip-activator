cc56da49baa9ede707d98b3378dda9612a0374e0
ramyb <rami@dudamobile.com>
2015-07-23 16:27:33 +0300

    advanced syntax

--- addon/mixins/bootstrap-tooltip-activator.js
+++ addon/mixins/bootstrap-tooltip-activator.js
@@ -1,55 +1,64 @@
 import Ember from 'ember';
 
-var $ = Ember.$;
-export default Ember.Mixin.create({
+const Em = Ember;
+
+const {
+	$, Mixin, A, on,
+	run: {
+		bind,
+		scheduleOnce
+	}
+} = Ember;
+
+export default Mixin.create({
 	timer: null,
 	interval: 500,
 	isActive: true,
 	cachedElements: null,
 
-	setCachedElements: Ember.on('init', function(){
-		this.set('cachedElements', Ember.A([]));
+	setCachedElements: on('init', function() {
+		this.set('cachedElements', Em.A([]));
 	}),
 
-	initTooltips: Ember.on('didInsertElement', function(){
-		Ember.run.scheduleOnce('afterRender', this, function(){
-			var cachedElements = this.get('cachedElements');
-			if (this.get('isActive') && this.get('_state') === 'inDOM'){
+	initTooltips: on('didInsertElement', function() {
+		scheduleOnce('afterRender', this, function() {
+			let cachedElements = this.get('cachedElements');
+			if (this.get('isActive') && this.get('_state') === 'inDOM') {
 				this.clearTooltipsOfDetachedElements();
-				this.$('[data-toggle="tooltip"]').toArray().forEach(function(el){
-					var $el = $(el),
+				this.$('[data-toggle="tooltip"]').toArray().forEach(function(el) {
+					let $el = $(el),
 						tooltipData = $el.data()['bs.tooltip'];
-					if (!tooltipData){
+					if (!tooltipData) {
 						$el.tooltip();
 						cachedElements.pushObject($el);
 					}
 				}, this);
-				if (!this.get('timer')){
-					this.set('timer', window.setInterval(Ember.run.bind(this, this.initTooltips), this.get('interval')));
+				if (!this.get('timer')) {
+					this.set('timer', setInterval(bind(this, this.initTooltips), this.get('interval')));
 				}
 			}
 		});
 	}),
 
-	clearTooltipsOfDetachedElements: function(){
-		var cachedElements = this.get('cachedElements'),
-			zombies = cachedElements.filter(function(el){
-				return !Ember.$.contains(window.document, el[0]);
+	clearTooltipsOfDetachedElements: function() {
+		let cachedElements = this.get('cachedElements'),
+			zombies = cachedElements.filter(function(el) {
+				return !$.contains(document, el[0]);
 			});
 
-		zombies.forEach(function(el){
+		zombies.forEach(function(el) {
 			el.tooltip('destroy');
 			cachedElements.removeObject(el);
 			el.remove();
 		});
 	},
 
-	destroyTooltips: Ember.on('willDestroyElement', function(){
+	destroyTooltips: on('willDestroyElement', function() {
 		this._super();
 		this.clearTooltipsOfDetachedElements();
 		this.set('isActive', false);
-		window.clearInterval(this.get('timer'));
+		clearInterval(this.get('timer'));
 		this.$('[data-toggle="tooltip"]').tooltip('destroy');
 	})
-	
+
 });
